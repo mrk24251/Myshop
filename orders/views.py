f5bdba93762f2e85264beb471bdd9b7cb2a63145
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2020-09-28 19:37:59 +0330

    4rd attempt for debuaging payment page

--- orders/views.py
+++ orders/views.py
@@ -60,14 +60,14 @@ def order_create(request):
             # clear the cart
             cart.clear()
             # launch asynchronous task
-            order_created.delay(order.id)
+            # order_created.delay(order.id)
             # set the order in the session
             request.session['order_id'] = order.id
             # redirect for payment
             if request.session['currency'] == "IRR":
-                return redirect(reverse('orders:create'))
+                return redirect(reverse('payment:request'))
             else:
-                return redirect(reverse('orders:create'))
+                return redirect(reverse('payment:process'))
 
     else:
         form = OrderCreateForm()
